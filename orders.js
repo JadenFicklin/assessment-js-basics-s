890aff36160a4f1ecc4e85a7efb89ce07d0a9739
JadenFicklin <jadendficklin@gmail.com>
2021-12-23 16:25:33 -0700

    Completed orders.js

--- orders.js
+++ orders.js
@@ -6,7 +6,6 @@
     to make tickets from order information.
 */
 
-
 //////////////////PROBLEM 1////////////////////
 /*  
     Create a new class called `Ticket`.
@@ -30,8 +29,19 @@
 */
 
 //CODE HERE
-
-
+class Ticket {
+  constructor(items, orderTime, CustomerId) {
+    this.items = items;
+    this.orderTime = orderTime;
+    this.CustomerId = CustomerId;
+    this.status = "queued";
+  }
+  updateStatus(newStatus) {
+    this.status = newStatus;
+    console.log(`'The order for customer ${this.CustomerId} is
+    now ${this.status}.`);
+  }
+}
 
 /*
     Create a new instance of your class.
@@ -45,8 +55,8 @@
 */
 
 //CODE HERE
-
-
+const firstTicket = new Ticket("pizza, bread, and soda", "7:03 PM", 575);
+console.log(firstTicket);
 /*
     Call the `updateStatus` method on
     `firstTicket` passing in the string
@@ -54,3 +64,4 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus("cooking");
